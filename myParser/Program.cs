60079a5abf3186b686af4dcb08b6bd8a965f74e6
VToribiot <80557014+VToribiot@users.noreply.github.com>
2022-11-17 20:23:50 -0400

    Add files via upload

---
+++ myParser/Program.cs
@@ -0,0 +1,16 @@
+﻿using Antlr4.Runtime;
+using myParser;
+
+
+
+string input = "1+5*8+6";
+Console.WriteLine($"Input {input}\n");
+
+ICharStream stream = CharStreams.fromString(input);
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+CommonTokenStream tokens = new CommonTokenStream(lexer);
+aritmeticaParser parser = new aritmeticaParser(tokens);
+var tree = parser.aritmetica();
+aritmetica aritmetica = new aritmetica();
+
+int resultado = aritmetica.Visit(tree);
---
+++ myParser/aritmetica.cs
@@ -0,0 +1,40 @@
+﻿using Antlr4.Runtime.Misc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace myParser
+{
+    internal class aritmetica : aritmeticaBaseVisitor<int>
+    {
+		public override int VisitAritmetica([NotNull] aritmeticaParser.AritmeticaContext context)
+		{
+			Console.WriteLine($"Visitando Aritmetica: {context.GetText()}");
+			Console.WriteLine("-------------------------");
+			return base.VisitAritmetica(context);
+		}
+
+		public override int VisitExpresion([NotNull] aritmeticaParser.ExpresionContext context)
+		{
+			Console.WriteLine($"Visitando Expresion: {context.GetText()}");
+			Console.WriteLine("-------------------------");
+			return base.VisitExpresion(context);
+		}
+
+		public override int VisitTermino([NotNull] aritmeticaParser.TerminoContext context)
+		{
+			Console.WriteLine($"Visitando Termino: {context.GetText()}");
+			Console.WriteLine("-------------------------");
+			return base.VisitTermino(context);
+		}
+		public override int VisitFactor([NotNull] aritmeticaParser.FactorContext context)
+		{
+			Console.WriteLine($"Visitando Factor: {context.GetText()}");
+			Console.WriteLine("-------------------------");
+			return base.VisitFactor(context);
+		}
+	}
+}
